10adb9c9c07a9cec2767a4654dfd6951c715315e
Henry Chang <mr.changyuheng@gmail.com>
2018-10-06 10:51:03 +0800

    Switch to Mozilla Public License 2.0 (MPL-2.0)

--- zsh-interactive-cd.plugin.zsh
+++ zsh-interactive-cd.plugin.zsh
@@ -1,4 +1,10 @@
 #!/usr/bin/env zsh
+#
+# Copyright 2017-2018 Henry Chang
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 __zic_fzf_prog() {
   [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX:-0}" != 0 ] && [ ${LINES:-40} -gt 15 ] \
